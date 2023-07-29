a4a2013c34db1f5e5151e9e2824c88d3372e37c5
Oussama Taali <oussamataali1997@gmail.com>
2023-07-29 18:23:39 +0100

    make search dynamic

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,20 +4,22 @@ import {BiCategoryAlt} from 'react-icons/bi'
 import {MdOutlineLocationOn} from 'react-icons/md'
 const Search = () => {
 
-  const city =['Tanger','Errachidia','Casa','Goulmima','Méknes','Tetouan','Mohamedia','Asfi','Tinghir','Fes','Marakech','Dakhla','Laayoun']
+  const city =['Errissani','Tadighoust','Goulmim','Tileft','Tanger','Errachidia','Casa','Goulmima','Méknes','Tetouan','Mohamedia','Asfi','Tinghir','Fes','Marakech','Dakhla','Laayoun']
 
-  const [acctive,setAcctive]=useState(false)
+  const [acctive,setAcctive]=useState(true)
   const [cityy,setCity]=useState('')
 
   const handlefocus =()=>{
     setAcctive(true)
   }
 
-  const handleClick =(e)=>{
-console.log(e)
-setCity(e.target.innerHTML)
-setAcctive(false)
+  const handleClck = (e) =>{
+    setCity(e.target.innerHTML)
+    setAcctive(!acctive)
   }
+
+
+  console.log(cityy)
 
   return (
     <div className='flex  flex-col md:flex-row gap-3 md:bg-white  p-6 max-w-5xl md:mx-auto md:shadow-2xl mx-1 -mt-[150px] md:-mt-14'>
@@ -41,11 +43,12 @@ setAcctive(false)
         <label className='font-bold text-sm hidden md:block' htmlFor="Ville">Choisissez une ville</label>
         <div className="relative flex items-center ">
         <MdOutlineLocationOn className='absolute ml-2'/>
-        <input type="text"  className=' border pl-7 p-2 w-full focus:outline-none' onChange={(e)=>setCity(e.target.value)} value={cityy ? cityy : ''}  autoComplete="off"  onFocus={handlefocus}  placeholder='Tout le Maroc' id='search' name='search'/>
-        {acctive && <div data-aos="fade-up"   data-aos-delay="1"
-    data-aos-duration="9000" className=" bg-white absolute top-10 w-full leading-9 divide-y-[1px] h-[14rem] overflow-auto">
-            {city.map((c,index)=>(
-              <p key={index} className='font-light text-md cursor-pointer px-2 hover:bg-gray-100' onClick={handleClick}>{c}</p>
+        <input type="text"  className=' border pl-7 p-2 w-full focus:outline-none' onClick={()=>setAcctive(true)} onChange={(e)=>setCity(e.target.value)} value={cityy}  autoComplete="off"  placeholder='Tout le Maroc' id='search' name='search'/>
+        { acctive && <div data-aos="fade-up"   data-aos-delay="1"
+    data-aos-duration="9000" className="  bg-white absolute top-10 w-full leading-9 divide-y-[1px] h-[14rem] overflow-auto">
+            {city.filter(e=>e.toLowerCase().startsWith(cityy.toLowerCase())
+).map((c,index)=>(
+              <p key={index} className='font-light text-md cursor-pointer px-2 hover:bg-gray-100' onClick={handleClck}>{c}</p>
             ))}
         </div> }
         </div>
